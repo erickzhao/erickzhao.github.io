ee38d191c35710be6ea6a9c694eeee12305c0fb5
erick <erick@hotmail.ca>
2017-03-11 20:07:18 -0500

    Add skills icons

--- app/components/about.controller.js
+++ app/components/about.controller.js
@@ -7,4 +7,22 @@ function AboutController(){
 	var vm = this;
 
 	vm.test = "This is my about page";
+
+
+	vm.skills = [
+					"html5",
+					"javascript",
+					"jquery",
+					"git",
+					"angularjs",
+					"d3js",
+					"css3",
+					"sass",
+					"bootstrap",
+					"grunt",
+					"java",
+					"photoshop",
+					"python",
+					"c",
+					"csharp"]
 }
