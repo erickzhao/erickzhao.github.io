e514daab33a0e8fcc8ed71c15379dda2fbca92a7
erick <erick@hotmail.ca>
2018-01-06 13:46:12 -0500

    Calculate center for parallax instead of offset

--- src/js/parallax.js
+++ src/js/parallax.js
@@ -4,12 +4,10 @@ const hero = document.getElementById('hero');
 
 hero.addEventListener('mousemove', (e) => {
   const factor = 15;
-  const offsetX = 500;
-  const offsetY = 200;
   const posX = (e.pageX);
   const posY = (e.pageY);
 
   parallax.forEach((p, i) => {
-    p.style.backgroundPosition = `${offsetX - (posX / ((i + 1) * factor))}px ${offsetY - (posY / ((i + 1) * factor))}px`; // eslint-disable-line no-param-reassign
+    p.style.backgroundPosition = `calc(50% - ${(posX / ((i + 1) * factor))}px) calc(50% - ${(posY / ((i + 1) * factor))}px)`; // eslint-disable-line no-param-reassign
   });
 });
