609171695a1baad61d41929a5606267c16fd642b
erick <erick@hotmail.ca>
2018-01-05 23:04:39 -0500

    Add typing

---
+++ src/js/typing.js
@@ -0,0 +1,8 @@
+const Typed = require('typed.js');
+
+const type = new Typed('#typed', { // eslint-disable-line no-unused-vars
+  stringsElement: '#typed-strings',
+  smartBackspace: true,
+  loop: true,
+  typeSpeed: 35,
+});
