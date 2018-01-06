a66a71a1fcb72d265f9e3fab2d88ff1455e6110f
erick <erick@hotmail.ca>
2018-01-06 10:28:35 -0500

    Slightly better parallax

--- src/js/parallax.js
+++ src/js/parallax.js
@@ -4,12 +4,12 @@ const hero = document.getElementById('hero');
 
 hero.addEventListener('mousemove', (e) => {
   const factor = 15;
-  const offsetX = document.documentElement.clientWidth / 4;
-  const offsetY = document.documentElement.clientHeight / 4;
+  const offsetX = 500;
+  const offsetY = 200;
   const posX = (e.pageX);
   const posY = (e.pageY);
 
   parallax.forEach((p, i) => {
-    p.style.backgroundPosition = `${-offsetX - (posX / ((i + 1) * factor))}px ${-offsetY - (posY / ((i + 1) * factor))}px`; // eslint-disable-line no-param-reassign
+    p.style.backgroundPosition = `${offsetX - (posX / ((i + 1) * factor))}px ${offsetY - (posY / ((i + 1) * factor))}px`; // eslint-disable-line no-param-reassign
   });
 });
