e109eecd52599850d8034e843c12d28564027745
erick <erick@hotmail.ca>
2018-01-08 17:03:52 -0500

    Add lazy load animation to timeline

---
+++ src/js/animations.js
@@ -0,0 +1,32 @@
+const handleIntersect = (entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.remove('hidden');
+      entry.target.classList.add('animated', 'bounceIn');
+    }
+  });
+};
+
+const createObserver = (elements) => {
+  const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.5,
+  };
+
+  if (IntersectionObserver) {
+    const observer = new IntersectionObserver(handleIntersect, options);
+    elements.forEach((el) => {
+      el.classList.add('hidden');
+      observer.observe(el);
+    });
+  }
+};
+
+window.addEventListener('load', () => {
+  const elements = document.querySelectorAll('.timeline--block');
+  if (IntersectionObserver) {
+    createObserver(elements);
+  }
+}, false);
+
--- src/js/app.js
+++ src/js/app.js
@@ -2,3 +2,4 @@ require('smoothscroll');
 require('./typing');
 require('./parallax');
 require('./project-nav');
+require('./animations');
