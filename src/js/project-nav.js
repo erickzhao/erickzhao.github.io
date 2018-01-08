5e1a5b244a434f0b7c0eabaad14ee14875f089de
erick <erick@hotmail.ca>
2018-01-08 16:10:35 -0500

    Add project nav with arrows

--- src/js/project-nav.js
+++ src/js/project-nav.js
@@ -1,16 +1,23 @@
 const smoothScroll = require('smoothscroll');
 
 const upBtn = document.getElementById('project--up');
-const downBtn = document.getElementById('project-down');
+const downBtn = document.getElementById('project--down');
 const projects = document.querySelectorAll('.project');
+let activeIndex = 0;
 
-// upBtn.addEventListener('click', (e) => {
+upBtn.addEventListener('click', () => {
+  if (activeIndex > 0) {
+    smoothScroll(projects[activeIndex - 1]);
+    activeIndex -= 1;
+  }
+});
 
-// });
-
-// downBtn.addEventListener('click', (e) => {
-
-// });
+downBtn.addEventListener('click', () => {
+  if (activeIndex < projects.length - 1) {
+    smoothScroll(projects[activeIndex + 1]);
+    activeIndex += 1;
+  }
+});
 
 const getActiveProject = () => {
   for (let i = projects.length - 1; i >= 0; i--) { // eslint-disable-line no-plusplus
@@ -29,9 +36,10 @@ const setActiveAnchor = () => {
   const projAnchors = document.querySelectorAll('a[href^="#project--"]');
   const activeProj = document.querySelector(`a[href='#${proj.id}']`);
 
-  projAnchors.forEach((anchor) => {
+  projAnchors.forEach((anchor, index) => {
     if (anchor === activeProj) {
       anchor.classList.add('active');
+      activeIndex = index;
     } else {
       anchor.classList.remove('active');
     }
